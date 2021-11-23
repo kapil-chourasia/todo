d99d90605f27d88eeca457701af143e69d8d7584
Kapil Chouasia <kapilc5303@gmail.com>
2021-11-23 15:42:12 +0530

    commit on master

--- src/app/service/basic-authentication.service.ts
+++ src/app/service/basic-authentication.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { API_URL } from './../app.constants';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { API_URL } from '../app.constants';
+import {map} from 'rxjs/operators';
 
 export const TOKEN = 'token'
 export const AUTHENTICATED_USER = 'authenticaterUser'
@@ -13,34 +13,45 @@ export class BasicAuthenticationService {
 
   constructor(private http: HttpClient) { }
 
-  // authenticate(username: string, password: any) {
-
-  //   //console.log('before ' + this.isUserLoggedIn());
-  //   if (username === "kapil" && password === 'kapil') {
-  //     sessionStorage.setItem('authenticatedUser', username);
-  //     //console.log('after ' + this.isUserLoggedIn());
-  //     return true;
-  //   }
-  //   return false;
-  // }
-
-  executeAuthenticationService(username:string, password:any) {
-    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
-    let headers = new HttpHeaders({Authorization: basicAuthHeaderString})
-
-    return this.http.get<AuthenticationBean>(
-      `${API_URL}/basicauth`,
-      { headers }).pipe(
+  executeJWTAuthenticationService(username, password) {
+    
+    return this.http.post<any>(
+      `${API_URL}/authenticate`,{
+        username,
+        password
+      }).pipe(
         map(
           data => {
             sessionStorage.setItem(AUTHENTICATED_USER, username);
-            sessionStorage.setItem(TOKEN, basicAuthHeaderString);
-
+            sessionStorage.setItem(TOKEN, `Bearer ${data.token}`);
             return data;
           }
         )
       );
+    //console.log("Execute Hello World Bean Service")
+  }
 
+
+  executeAuthenticationService(username, password) {
+    
+    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+
+    let headers = new HttpHeaders({
+        Authorization: basicAuthHeaderString
+      })
+
+    return this.http.get<AuthenticationBean>(
+      `${API_URL}/basicauth`,
+      {headers}).pipe(
+        map(
+          data => {
+            sessionStorage.setItem(AUTHENTICATED_USER, username);
+            sessionStorage.setItem(TOKEN, basicAuthHeaderString);
+            return data;
+          }
+        )
+      );
+    //console.log("Execute Hello World Bean Service")
   }
 
   getAuthenticatedUser() {
@@ -48,23 +59,22 @@ export class BasicAuthenticationService {
   }
 
   getAuthenticatedToken() {
-    if (this.getAuthenticatedUser())
+    if(this.getAuthenticatedUser())
       return sessionStorage.getItem(TOKEN)
   }
-
 
   isUserLoggedIn() {
     let user = sessionStorage.getItem(AUTHENTICATED_USER)
     return !(user === null)
   }
 
-  logout() {
+  logout(){
     sessionStorage.removeItem(AUTHENTICATED_USER)
     sessionStorage.removeItem(TOKEN)
   }
 
 }
 
-export class AuthenticationBean {
-  constructor(public message: string) { }
+export class AuthenticationBean{
+  constructor(public message:string) { }
 }
--- src/app/service/route-guard.service.ts
+++ src/app/service/route-guard.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { HardcodedAuthenticationService } from './hardcoded-authentication.service';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +8,18 @@ import { HardcodedAuthenticationService } from './hardcoded-authentication.servi
 export class RouteGuardService implements CanActivate {
 
   constructor(
-    private hardcodedAuthenticationService: HardcodedAuthenticationService, 
-    private router: Router) { }
+    private hardcodedAuthenticationService: HardcodedAuthenticationService,
+    private router: Router) {
+
+  }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
 
     if (this.hardcodedAuthenticationService.isUserLoggedIn())
       return true;
-    
-      this.router.navigate(['login'])
+
+    this.router.navigate(['login']);
+
     return false;
   }
 }
